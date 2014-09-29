489445970b383d6656df81fcd27c26818dc43033
Anton Nikolaevsky <anton.nikolaevsky@itv.ru>
2014-09-29 15:51:46 +0400

    post-merge fix

--- libavutil/arm/intmath.h
+++ libavutil/arm/intmath.h
@@ -70,6 +70,7 @@ static av_always_inline av_const unsigned av_clip_uintp2_arm(int a, int p)
     __asm__ ("usat %0, %2, %1" : "=r"(x) : "r"(a), "i"(p));
     return x;
 }
+#endif //!CONFIG_SMALL
 
 #define av_sat_add32 av_sat_add32_arm
 static av_always_inline int av_sat_add32_arm(int a, int b)
